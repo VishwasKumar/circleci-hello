6101057c76d242b05675f4d58197fa98a47cc871
Vishwas Kumar <vishwaskumar58@gmail.com>
2020-05-31 19:53:07 -0400

    troubleshooting working_dir in circleci

--- src/admin/main_test.go
+++ src/admin/main_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestHelloFunc(t *testing.T) {
   t.Log("Checking that function prints what's expected...")
   result := GetHello()
-  if result != "Hello World!" {
-    t.Error("Expected 'Hello World!' but got", result)
+  if result != "Hello World From Admin!" {
+    t.Error("Expected 'Hello World From Admin!' but got", result)
   }
 }
