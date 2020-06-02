b91f734a5661fecce39aa235ce9ef01081625a17
Vishwas Kumar <vishwaskumar58@gmail.com>
2020-06-02 11:33:07 -0400

    test triggering app service

--- src/app/main.go
+++ src/app/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func GetHello() string {
-  return "Hello World From App!"
+  return "Hello World From App!!"
 }
 
 func main() {
--- src/app/main_test.go
+++ src/app/main_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestHelloFunc(t *testing.T) {
   t.Log("Checking that function prints what's expected...")
   result := GetHello()
-  if result != "Hello World From App!" {
-    t.Error("Expected 'Hello World From App!' but got", result)
+  if result != "Hello World From App!!" {
+    t.Error("Expected 'Hello World From App!!' but got", result)
   }
 }
